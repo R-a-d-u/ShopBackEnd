858d66fde6a320c40b601451f7d4df816b83fe6d
radu cruceat <radu.cruceat@gmail.com>
2025-07-30 18:45:17 +0300

    added gold history validation

---
+++ ShopBackEnds/Validation/GoldHistory/GoldHistoryAddValidation .cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using ShopBackEnd.Data.Dto;
+
+namespace ShopBackEnd.Validation.GoldHistory
+{
+    public class GoldHistoryAddValidation : AbstractValidator<GoldHistoryDtoAdd>
+    {
+        public GoldHistoryAddValidation()
+        {
+            RuleFor(x => x.Metal)
+                .NotEmpty()
+                .MaximumLength(255)
+                .WithMessage("Metal name is required and cannot exceed 255 characters.");
+
+            RuleFor(x => x.PriceOunce)
+                .GreaterThan(0)
+                .WithMessage("Price per ounce must be greater than 0.");
+
+            RuleFor(x => x.PriceGram)
+                .GreaterThan(0)
+                .WithMessage("Price per gram must be greater than 0.");
+
+            RuleFor(x => x.Exchange)
+                .NotEmpty()
+                .MaximumLength(255)
+                .WithMessage("Exchange name is required and cannot exceed 255 characters.");
+
+            RuleFor(x => x.Timestamp)
+                .NotEmpty()
+                .MaximumLength(255)
+                .WithMessage("Timestamp is required and cannot exceed 255 characters.");
+
+            RuleFor(x => x.Date)
+                .NotEmpty()
+                .WithMessage("Date is required.");
+        }
+    }
+}
---
+++ ShopBackEnds/Validation/GoldHistory/GoldHistoryIdValidation .cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ShopBackEnd.Validation.GoldHistory
+{
+    public class GoldHistoryIdValidation : AbstractValidator<int>
+    {
+        public GoldHistoryIdValidation()
+        {
+            RuleFor(id => id)
+                .GreaterThan(0)
+                .WithMessage("Gold history ID must be greater than 0.");
+        }
+    }
+}
