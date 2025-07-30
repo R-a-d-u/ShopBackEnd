fbaaf2ff0f719e7f493c31e4e67ec57b3fdf7096
radu cruceat <radu.cruceat@gmail.com>
2025-07-30 18:44:37 +0300

    added cart validation

---
+++ ShopBackEnds/Validation/CartValidation/CartIdValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ShopBackEnd.Data.Dto;
+
+namespace ShopBackEnd.Validation.CartValidation
+{
+    public class CartIdValidation : AbstractValidator<int>
+    {
+        public CartIdValidation()
+        {
+            RuleFor(id => id)
+                .GreaterThan(0).WithMessage("Cart ID must be a positive integer.");
+        }
+    }
+
+}
