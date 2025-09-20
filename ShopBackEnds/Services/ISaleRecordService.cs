f4dffd584eb789ca98b57aeaa0dee2d52b353061
radu cruceat <radu.cruceat@gmail.com>
2025-09-20 20:11:55 +0300

    added sale record service

---
+++ ShopBackEnds/Services/ISaleRecordService.cs
@@ -0,0 +1,13 @@
+﻿using ShopBackEnd.Data.Dto;
+
+namespace ShopBackEnd.Services
+{
+    public interface ISaleRecordService
+    {
+        Task<List<ProductSalesSummaryDto>> GetProductSalesSummaryBetweenDates(DateTime startDate, DateTime endDate);
+        Task<List<HourlySalesSummaryDto>> GetHourlySalesSummary(DateTime startDate, DateTime endDate);
+        Task<RevenueAnalysisDto> GetRevenueAnalysis(DateTime startDate, DateTime endDate);
+        Task<CustomerPurchasePatternDto> GetCustomerPurchasePatterns(int userId);
+        Task<List<CategorySalesDto>> GetCategorySalesPerformance(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ ShopBackEnds/Services/SaleRecordService .cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using ShopBackEnd.Data.Dto;
+using ShopBackEnd.Data.Entity;
+using ShopBackEnd.Repository.EFCoreRepositories;
+using ShopBackEnd.Validation.UserValidation;
+
+namespace ShopBackEnd.Services
+{
+    public class SaleRecordService : ISaleRecordService
+    {
+        private readonly ISaleRecordRepository _saleRecordRepository;
+        private readonly UserIdValidation _userIdValidation;
+
+        public SaleRecordService(ISaleRecordRepository saleRecordRepository,UserIdValidation userIdValidation)
+        {
+            _saleRecordRepository = saleRecordRepository;
+            _userIdValidation= userIdValidation;
+        }
+
+        public async Task<List<ProductSalesSummaryDto>> GetProductSalesSummaryBetweenDates(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date must be before or equal to end date.");
+            }
+
+            return await _saleRecordRepository.GetProductSalesSummaryBetweenDates(startDate, endDate);
+        }
+        public async Task<List<HourlySalesSummaryDto>> GetHourlySalesSummary(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date must be before or equal to end date.");
+            }
+
+            return await _saleRecordRepository.GetHourlySalesSummary(startDate, endDate);
+        }
+        public async Task<RevenueAnalysisDto> GetRevenueAnalysis(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date must be before or equal to end date.");
+            }
+
+            return await _saleRecordRepository.GetRevenueAnalysis(startDate, endDate);
+        }
+        public async Task<CustomerPurchasePatternDto> GetCustomerPurchasePatterns(int userId)
+        {
+            var validationResult = _userIdValidation.Validate(userId);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+            return await _saleRecordRepository.GetCustomerPurchasePattern(userId);
+        }
+        public async Task<List<CategorySalesDto>> GetCategorySalesPerformance(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                throw new ArgumentException("Start date must be before or equal to end date.");
+            }
+
+            return await _saleRecordRepository.GetCategorySalesPerformance(startDate, endDate);
+        }
+    }
+}
